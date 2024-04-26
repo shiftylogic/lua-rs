906df0e63b5ad70ebb7d98a42b133a59cf90fa19
Robert Anderson <github@shiftylogic.com>
2024-04-26 09:48:58 -0700

    Initial commit

---
+++ examples/basic.rs
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2024-present Robert Anderson.
+ * SPDX-License-Identifier: MIT
+ *
+ * Demonstrate basic embedding of the LUA run-time.
+ *
+ */
+
+fn main() {
+    lua_rs::do_it();
+}
---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2024-present Robert Anderson.
+ * SPDX-License-Identifier: MIT
+ *
+ * Game systems playground.
+ *
+ * Purpose:
+ *   Playing around with various game systems / libraries.
+ */
+
+pub fn do_it() {
+    println!("It is done!");
+}
